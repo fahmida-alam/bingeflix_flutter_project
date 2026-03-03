aaaa8a30fdbdae0c7b33d8a3f2bfc144f3b33de2
Fahmida Alam <fahmidasneha2020@gmail.com>
2026-03-03 13:27:10 +1100

    Update: Added AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,19 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'BINGEWORTHY TV SHOWS',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontFamily: 'BebasNeue', 
+            )
+          ),
+        backgroundColor: const Color.fromARGB(255, 184, 179, 179),
+        centerTitle: true,
+        ),
+        
         body: Container(
           color: Colors.teal,
           child: Center(
